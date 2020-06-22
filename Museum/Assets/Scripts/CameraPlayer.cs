1b6b297101c7aad14cdb57ccd9c424ab9ab81bf9
Kinderril <sarevok007@yandex.ru>
2020-06-22 19:36:00 +0300

    test pic

---
+++ Museum/Assets/Scripts/CameraPlayer.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraPlayer : MonoBehaviour
+{
+    public WindowHall Hall;
+
+    void Update()
+    {
+        if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out var hitInfo, 2.0f))
+        {
+            var obj = hitInfo.transform.GetComponent<PictureObject>();
+            if (obj != null)
+            {
+                Hall.SetInfo(obj.Info);
+            }
+            else
+            {
+                Hall.SetNull();
+            }
+        }
+        else
+        {
+            Hall.SetNull();
+        }
+
+
+    }
+}
---
+++ Museum/Assets/Scripts/WindowHall.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using TMPro;
+
+public class WindowHall : MonoBehaviour
+{
+    public TextMeshProUGUI TextField;
+    private float _setTime;
+
+    public void SetInfo(string objInfo)
+    {
+
+
+        if (!TextField.gameObject.activeSelf)
+        {
+            TextField.gameObject.SetActive(true);
+            TextField.text = objInfo;
+            _setTime = Time.time;
+        }
+    }
+
+    public void SetNull()
+    {
+        var delta = Time.time - _setTime;
+        if (delta > 1f)
+        {
+
+
+            if (TextField.gameObject.activeSelf)
+            {
+                TextField.gameObject.SetActive(false);
+            }
+        }
+
+    }
+}
